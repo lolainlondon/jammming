8289dc8a6770123da4e062ed075a7ac03f39aaff
lolainlondon <134396043+lolainlondon@users.noreply.github.com>
2023-08-07 14:58:31 +0100

    changed search to brian

--- src/Search/SearchBar.js
+++ src/Search/SearchBar.js
@@ -3,7 +3,7 @@ import react, {useState} from 'react'
 function SearchBar() {
     return (
         <form>
-            <label htmlFor="search">Search</label>
+            <label htmlFor="search">Brian</label>
             <input id="search" name="search" type="text"/>
         </form>
     )
